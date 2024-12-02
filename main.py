f5ee1b145385f99ae2210122c663d3b01bca7a5d
Sergei Shchepa <sergei.shchepa@coolblue.nl>
2024-12-02 18:11:08 +0100

    Difference of 4 between last 2 attempts, really?! Uh oh not proud of this one

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from tasks.day1 import compare_lists
 from tasks.day2 import safe_reports
 
 if __name__ == "__main__":
-    print(f"Day 1:\n  {compare_lists()}")
-    print(f"Day 2:\n  {safe_reports()}")
+    # print(f"Day 1:\n  {compare_lists()}")
+    # print(f"Day 2:\n  {safe_reports()}")
+    print(f"Day 2:\n  {safe_reports(True)}")
--- tasks/day2.py
+++ tasks/day2.py
@@ -1,4 +1,5 @@
 import re
+from copy import copy
 from itertools import pairwise
 from .utils import NUMBERS_PATTERN, get_file
 
@@ -9,25 +10,36 @@ def load_data():
         data.append([int(x) for x in re.findall(NUMBERS_PATTERN, line)])
     return data
 
+def is_safe(report) -> bool:
+    asc = False
+    desc = False
+    for pair in pairwise(report):
+        diff = pair[0] - pair[1]
+        if not (3 >= abs(diff) >= 1):
+            asc = desc = True
+            return False
+        if diff > 0:
+            desc = True
+        elif diff < 0:
+            asc = True
+        else:
+            asc = desc = True
+    if asc == desc:
+        return False
+    return True 
+
 def safe_reports(with_dampener=False):
     data = load_data()
     safe = 0
 
     for report in data:
-        asc = False
-        desc = False
-        for pair in pairwise(report):
-            diff = pair[0] - pair[1]
-            if not (3 >= abs(diff) >= 1):
-                asc = desc = True
-                break
-
-            if diff > 0:
-                desc = True
-            elif diff < 0:
-                asc = True
-            else:
-                asc = desc = True
-        if asc != desc:
-            safe +=1 
+        if is_safe(report=report):
+            safe += 1
+        elif with_dampener:
+            for idx, _ in enumerate(report):
+                dampened = copy(report)
+                del(dampened[idx])
+                if is_safe(dampened):
+                    safe += 1
+                    break
     return safe
